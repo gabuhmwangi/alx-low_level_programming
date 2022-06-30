c4f01d8c3e1cd7fa8e2b8ff2c82b82f4261ad452
Gabriel Mwangi <gabuhmwangi@gmail.com>
2022-06-30 12:58:17 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
  *  main - Entry point
+ *
+ *  Description: A C program that prints with printf function.
+ *
  *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");        return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
